f037491ac1e3b5827350e46cf49b7517f80b8c62
GerdHirsch <kontakt@gerdhirsch.de>
2016-11-12 18:48:01 +0100

    folder renamed

---
+++ VisitorFramework/include/VisitorAcyclic.h
@@ -0,0 +1,152 @@
+/*
+ * AcyclicVisitor.h
+ *
+ *  Created on: 10.11.2016
+ *      Author: Gerd
+ */
+
+#ifndef VISITORACYCLIC_H_
+#define VISITORACYCLIC_H_
+
+#include <string>
+#include <iostream>
+
+#include "ElementVisitor.h"
+
+namespace VisitorAcyclic{
+
+struct Visitor{
+	virtual ~Visitor(){}
+	virtual std::string toString() const = 0;
+};
+
+class Visitable
+{
+public:
+    virtual ~Visitable(){};
+	virtual void accept(Visitor& visitor) = 0;
+};
+//=====================================================================
+
+/**
+ * VisitableImpl stellt eine allgemeine Implementierung
+ * für alle ConcreteVisitables zur Verfügung.
+ * Dazu gehört:
+ * eine accept(::Visitor&) Methode
+ * für alle Visitables -> Implementation Inheritance
+ * Wenn die Ausgaben entfernt werden, bleibt eine einfache TemplateMethod
+ * (siehe GoF TemplateMethod Pattern) mit folgender Struktur übrig:
+ * cast des Visitors auf einen speziellen ElementVisitor
+ * wenn das möglich ist v.visit(this)
+ * ansonsten log("not accepted")
+ *
+ * log steht als policy zur Verfügung
+ *
+ * Außerdem definiert VisitableImpl das Interface Visitor
+ * für die ConcreteVisitors auf der Basis des template ElementVisitor
+ */
+template<
+	class ConcreteVisitable_,
+	class VisitableImplementation_,
+	class LoggingPolicy
+	>
+class VisitableImpl : public Visitable, public LoggingPolicy{
+
+public:
+	// Default Accessor für ElementVisitor,
+	// kann von einem konkreten Visitable überschrieben werden
+	class Accessor{};
+
+	// Parametertyp für visit in ElementVisitor
+	using ConcreteVisitable = ConcreteVisitable_;
+
+	// Typ der das Interface Visitable implementiert!
+	// Muss von VisitableImpl<..> erben!
+	// stellt Infrastruktur für ElementVisitor zur Verfügung:
+	// Accessor, ConcreteVisitable
+	// kann vom Parameter von visit(..) abweichen (Adapter)
+	using VisitableImplementation = VisitableImplementation_;
+
+	// Interface Definition für die konkreten Visitors
+	using Visitor = ElementVisitor<VisitableImplementation>;
+
+	// Muss überschrieben werden wenn ConcreteVisitable und
+	// VisitableImplementation nicht übereinstimmen
+	ConcreteVisitable* getVisitable() {
+		return static_cast<ConcreteVisitable*>(this);
+	}
+	ConcreteVisitable const* getVisitable() const {
+		return static_cast<ConcreteVisitable*>(this);
+	}
+
+	// liefert this als Pointer auf die Spezialisierung
+	VisitableImplementation* This(){
+		return static_cast<VisitableImplementation*>(this);
+	}
+
+	void accept(VisitorAcyclic::Visitor& visitor){
+		using Visitor = typename VisitableImplementation::Visitor;
+		Visitor* v = dynamic_cast<Visitor*>(&visitor); //crosscast
+//		ConcreteVisitable* visitable = This()->getVisitable();
+
+		decltype(auto) visitable = This()->getVisitable();
+		// visitable kann z.B. shared_ptr<ConcreteVisitable> sein
+		// der nicht mehr gültig ist
+		// TODO: einfach nix machen ist das elegant?
+		if(!visitable) return;
+
+		if(v){
+			this->logAccepted(*visitable, visitor);
+			v->visit(*visitable);
+		} else {
+			this->logNotAccepted(*visitable, visitor);
+		}
+	}
+};
+
+//=====================================================================
+//template<
+//	class ConcreteVisitable_,
+//	class VisitableImplementation_,
+//	class LoggingPolicy
+//	>
+//class VisitableImpl : public Visitable, public LoggingPolicy {
+//public:
+//	typedef ConcreteVisitable_ ConcreteVisitable;
+//	typedef ConcreteVisitable_ VisitableImplementation;
+//
+//	class Accessor{};
+//	using Visitor = ElementVisitor<VisitableImplementation>;
+//
+////	struct Visitor{
+////		virtual void visit(ConcreteVisitable& d) = 0;
+////	};
+//	// liefert this als Pointer auf die Spezialisierung
+//	VisitableImplementation* This(){
+//		return static_cast<VisitableImplementation*>(this);
+//	}
+//	// Muss überschrieben werden wenn ConcreteVisitable und
+//	// VisitableImplementation nicht übereinstimmen
+//	ConcreteVisitable* getVisitable() {
+//		return static_cast<ConcreteVisitable*>(this);
+//	}
+//	ConcreteVisitable const* getVisitable() const {
+//		return static_cast<ConcreteVisitable*>(this);
+//	}
+//
+//	void accept(VisitorAcyclic::Visitor& visitor){
+//		ConcreteVisitable *visitable = This()->getVisitable();
+//		Visitor *pV = dynamic_cast<Visitor*>(&visitor);
+//		if(pV){
+//			this->logAccepted(*visitable, visitor);
+//			pV->visit(*visitable);
+//		}else{
+//			this->logNotAccepted(*visitable, visitor);
+//		}
+//	}
+//};
+} // end namespace
+
+
+
+#endif /* VISITORACYCLIC_H_ */
---
+++ VisitorFramework/include/VisitorImplementation.h
@@ -0,0 +1,89 @@
+/*
+ * VisitorImplementation.h
+ *
+ *  Created on: 29.05.2016
+ *      Author: Gerd
+ */
+
+#ifndef VISITORIMPLEMENTATION_H_
+#define VISITORIMPLEMENTATION_H_
+
+#include "VisitorAcyclic.h"
+#include "MetaFunctions.h"
+
+struct EmptyAccessor{
+	std::string toString(){ return "EmptyAccessor";}
+};
+/*
+ * Adapter erzeugt einen Visitor für ein Element
+ * so dass ein ConcreteVisitor davon erben kann
+ * und über den Accessor, wenn vorhanden,
+ * auf die entsprechenden Member in dem jeweiligen
+ * besuchten Visitable zugreifen kann
+ */
+template<class ToVisit, class Accessor_ = EmptyAccessor>
+class Adapter{
+public:
+	std::string toString(){ return "VisitableAdapter";}
+
+	void print(){ std::cout << "VisitableAdapter" << std::endl; }
+	using ConcreteVisitable = ToVisit;
+	using Accessor = Accessor_;
+	using this_type = Adapter<ToVisit, Accessor>;
+	using Visitor = ElementVisitor<this_type>;
+};
+
+template<class T, class = void>
+struct hasVisitor{
+	enum {value = false};
+	using type = void;
+};
+template<class T>
+struct hasVisitor<T, typename void_type<typename T::Visitor>::type>{
+	enum {value = true};
+	using type = typename T::Visitor;
+};
+
+template<class T, class = void>
+struct hasAccessor{
+	enum {value = false};
+	using type = void;
+};
+template<class T>
+struct hasAccessor<T, typename void_type<typename T::Accessor>::type>{
+	enum {value = true};
+	using type = typename T::Accessor;
+};
+//---------------------------------------------------------------------
+template<class ToVisit, class =
+								IF<	hasVisitor<ToVisit>::value,
+									typename hasVisitor<ToVisit>::type,
+								IF<	hasAccessor<ToVisit>::value,
+									typename hasAccessor<ToVisit>::type,
+									EmptyAccessor>
+								>
+		>
+struct getVisitorType{
+	// kein Visitor und Accessor in ToVisit verfügbar
+	using type = typename Adapter<ToVisit>::Visitor;
+};
+template<class ToVisit>
+struct getVisitorType<ToVisit, typename hasVisitor<ToVisit>::type>{
+	// ToVisit definiert seinen Visitor
+	using type = typename hasVisitor<ToVisit>::type;
+
+};
+template<class ToVisit>
+struct getVisitorType<ToVisit, typename hasAccessor<ToVisit>::type>{
+	// ToVisit definiert nur einen Accessor
+	using type = typename Adapter<ToVisit, typename ToVisit::Accessor>::Visitor;
+};
+
+template<typename ToVisit>
+using getVisitor = typename getVisitorType<ToVisit>::type;
+template<typename ToVisit>
+using implementsVisitor = typename getVisitorType<ToVisit>::type;
+
+
+
+#endif /* VISITORIMPLEMENTATION_H_ */
